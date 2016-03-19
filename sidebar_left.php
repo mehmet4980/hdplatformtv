07d0580f10709c6f2486f0e02aff1d9f6c6c580f
mehmet4980 <mehmet498000@gmail.com>
2016-03-19 10:15:25 +0200

    Delete sidebar_left.php

--- sidebar_left.php
+++
@@ -1,8 +0,0 @@
-<aside id="sidebar1">
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Left Sidebar") ) : ?>
-		<h2><?php _e("Categories", "SamTv"); ?></h2>		
-		<ul>
-			<?php wp_list_cats(); ?>
-		</ul>
-<?php endif; ?>
-</aside>
